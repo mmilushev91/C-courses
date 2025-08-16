be0bfe5823288783473447e39a5c25fbbdb87702
mmilushev91 <milushev_1990@abv.bg>
2025-08-16 11:02:50 +0300

    Create printNameAgeGender.c

---
+++ Basic/Exercises/printNameAgeGender.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+
+{
+    printf("Full Name: Marian Milushev\n");
+    printf("Age: 34\n");
+    printf("Gender: Male\n");
+    printf("Full Name: Marian Milushev\nAge: 34\nGender: Male\n");
+}
