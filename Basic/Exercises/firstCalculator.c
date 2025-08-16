69f2ef1ae8c28ff6543a6e3f5e2cd3dd6d501831
mmilushev91 <milushev_1990@abv.bg>
2025-08-16 17:02:07 +0300

    Create firstCalculator.c

---
+++ Basic/Exercises/firstCalculator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("%d + %d = %d\n", 5, 2, 5 + 2);
+    printf("%d - %d = %d\n", 5, 2, 5 - 2);
+    printf("%d * %d = %d\n", 5, 2, 5 * 2);
+    printf("%d / %d = %f\n", 5, 2, 5.0 / 2.0);
+    printf("%d %% %d = %d\n", 5, 2, 5 % 2);
+
+    return 0;
+}
