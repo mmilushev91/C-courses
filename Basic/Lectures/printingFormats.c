af4b3f49a8989e0721c88e46e4121f9da5d0ec0b
mmilushev91 <milushev_1990@abv.bg>
2025-08-16 15:47:49 +0300

    Create printingFormats.c

---
+++ Basic/Lectures/printingFormats.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("My average grade is: %d\n", 93.7);// Return strange number
+    printf("My average grade is: %.1f\n", 93.7); //%f is used for floating point numbers
+                                                // the number before f tells how many decimal places we want to be shown
+                                                //we have to put. before the number
+    return 0;
+}
