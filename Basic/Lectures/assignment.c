61cebaac618c3f429e6f4712b71d7fb49c22cc7d
mmilushev91 <milushev_1990@abv.bg>
2025-08-17 11:20:36 +0300

    Create assignment.c

---
+++ Basic/Lectures/assignment.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int gradeI = 100;
+    //int grade;
+    //grade = 100;   
+
+    int gradeII = 90;
+    //int grade;
+    //grade = 90;
+
+    int average = (gradeI + gradeII) / 2;
+    //int average;
+    //average = (gradeI + gradeII) / 2;
+
+    printf("Average grade is: %d\n", average);
+    return 0;
+}
