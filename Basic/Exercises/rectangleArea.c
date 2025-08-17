071e8508d598d18da79e9fbd7756671ad1477beb
mmilushev91 <milushev_1990@abv.bg>
2025-08-17 16:11:57 +0300

    Update rectangleArea.c

--- Basic/Exercises/rectangleArea.c
+++ Basic/Exercises/rectangleArea.c
@@ -5,13 +5,17 @@ int main()
     double height;
     double width;
     double area;
-
+    
+    printf("Enter height: ");
     scanf("%lf", &height);
+    
+    printf("Enter width: ");
     scanf("%lf", &width);
-
+    
     area = height * width;
-
-    printf("The rectangle are is: %.2lf\n", area);
+    
+    printf("The are is: %.2lf", area);
+    
 
     return 0;
 }
