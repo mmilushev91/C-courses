0db43159c83197a46276f49fe71380e5606b00b5
mmilushev91 <milushev_1990@abv.bg>
2025-08-18 06:43:38 +0300

    Update nThermOfArithmeticSeq.c

--- Basic/Exercises/nThermOfArithmeticSeq.c
+++ Basic/Exercises/nThermOfArithmeticSeq.c
@@ -1,10 +1,22 @@
+#include <stdio.h>
+
 int main()
 {
-    int d = 2, a1 = 1, n = 9, aN;
+    float a1, d, an;
+    int n;
     
-    aN = a1 + (n - 1) * d;
+    printf("Enter initial term: ");
+    scanf("%f", &a1);
     
-    printf("%d\n", aN);
+    printf("Enter n value: ");
+    scanf("%d", &n);
+    
+    printf("Enter difference: ");
+    scanf("%f", &d);
+    
+    an = a1 + (n - 1) * d;
+    
+    printf("The n-th element of the arithmetic sequence is: %f", an);
     
     return 0;
 }
