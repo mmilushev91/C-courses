6f000e0aaca614e63042592f6d79106020d6af30
mmilushev91 <milushev_1990@abv.bg>
2025-08-19 10:05:49 +0300

    Create greaterFromTwoFloatingNumbers.c

---
+++ Basic/Exercises/greaterFromTwoFloatingNumbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    float num1, num2;
+    
+    printf("Enter first number: ");
+    scanf("%f", &num1);
+    
+    printf("Enter second number: ");
+    scanf("%f", &num2);
+    
+    if (num1 > num2) {
+        printf("%.1f is greater than %.1f", num1, num2);
+    } else {
+        printf("%.1f is greater than %.1f", num2, num1);
+    }
+
+    return 0;
+}
