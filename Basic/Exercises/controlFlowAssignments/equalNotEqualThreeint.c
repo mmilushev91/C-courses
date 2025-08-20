ea5cba4e78fb9563d9f1dc1f10699e78ad8ee3d7
mmilushev91 <milushev_1990@abv.bg>
2025-08-20 12:03:33 +0300

    Create equalNotEqualThreeint.c

---
+++ Basic/Exercises/controlFlowAssignments/equalNotEqualThreeint.c
@@ -0,0 +1,17 @@
+int main()
+{
+    int num1, num2, num3;
+    
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+    
+    printf("Enter third number: ");
+    scanf("%d", &num3);
+    
+    printf("%s", (num1 == num2 && num2 == num3) ? "EQUAL" : "NOT EQUAL");
+    
+    return 0;
+}
