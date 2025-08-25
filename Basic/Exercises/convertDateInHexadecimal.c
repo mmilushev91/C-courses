2cbd0fefdd7ac7e2a385238df9bade5498579c7c
mmilushev91 <milushev_1990@abv.bg>
2025-08-25 21:58:39 +0300

    Create convertDateInHexadecimal.c

---
+++ Basic/Exercises/convertDateInHexadecimal.c
@@ -0,0 +1,20 @@
+int main()
+{
+    int day, month, year;
+    
+    scanf("Enter day: ");
+    scanf("%d", &day);
+    
+    scanf("Enter month: ");
+    scanf("%d", &month);
+    
+    scanf("Enter year: ");
+    scanf("%d", &year);
+    
+    
+    printf("Character day hexadecimal value: %X\n", day);
+    printf("Character month hexadecimal value: %X\n", month);
+    printf("Character year hexadecimal value: %X\n", year);
+
+    return 0;
+}
