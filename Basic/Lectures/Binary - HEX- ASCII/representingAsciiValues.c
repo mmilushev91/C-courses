c76e337696e523f61af5fa45804624001d1a029d
mmilushev91 <milushev_1990@abv.bg>
2025-08-25 21:34:12 +0300

    Create representingAsciiValues.c

---
+++ Basic/Lectures/Binary - HEX- ASCII/representingAsciiValues.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    char chr = 'c';
+    
+    printf("Character code: %c\n", chr);
+    printf("Character ASCII decimal value: %d\n", chr);
+    printf("Character ASCII hexadecimal value: %X\n", chr);
+
+    return 0;
+}
