da0dbf0c802319a70da9aff0b8ff44b34c572210
mmilushev91 <milushev_1990@abv.bg>
2025-08-27 11:13:35 +0300

    Create punishment.c

---
+++ Basic/Exercises/for/punishment.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 0; i < 1000; i++) {
+        printf("I am sorry for not listening to my AlphaTech. I will practice more next time!\n");
+    }
+    
+    return 0;
+}
