6d7d3e06435c4a4fe5d7409cc1866e98b69f714c
mmilushev91 <milushev_1990@abv.bg>
2025-08-27 21:10:58 +0300

    Create greeting.c

---
+++ Basic/Exercises/functions/greeting.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void greeting();
+
+int main()
+{
+    greeting();
+
+    return 0;
+}
+
+void greeting() 
+
+{
+    printf("Hello!\nWelcome to the best course ever in C programming language\nGood luck!");
+}
